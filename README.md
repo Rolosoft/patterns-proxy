3447f854f82ef76ba256fd9a51db396fbc4ee50c
Rowland O'Connor <rowland.oconnor@emailhippo.com>
2017-11-04 14:24:53 +0000

    Initial commit.

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# patterns-proxy
+# Proxy Pattern
+
+[![rolosoft_public_packages MyGet Build Status](https://www.myget.org/BuildSource/Badge/rolosoft_public_packages?identifier=80c93ea5-b48f-417d-8fdb-86cee0096945)](https://www.myget.org/)
+
+## About
+Common [proxy pattern](https://en.wikipedia.org/wiki/Proxy_pattern).
+
+## Installation
+```
+install-package Rsft.Lib.Patterns.Proxy
+```
---
+++ src/Components/Rsft.Lib.Patterns.Proxy/IProxy.cs
@@ -0,0 +1,47 @@
+﻿// <copyright file="IProxy.cs" company="Rolosoft Ltd">
+// Copyright (c) Rolosoft Ltd. All rights reserved.
+// </copyright>
+
+// Copyright 2017 Rolosoft Ltd
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+namespace Rsft.Lib.Patterns.Proxy
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+    using JetBrains.Annotations;
+
+    /// <summary>
+    /// Proxy pattern.
+    /// </summary>
+    /// <seealso cref="https://en.wikipedia.org/wiki/Proxy_pattern"/>
+    /// <typeparam name="TParams">The type of the parameters.</typeparam>
+    /// <typeparam name="TReturn">The type of the return.</typeparam>
+    public interface IProxy<in TParams, TReturn>
+    {
+        /// <summary>
+        /// Executes the proxy asynchronously with the specified parameters.
+        /// </summary>
+        /// <param name="parameters">The parameters (if any).</param>
+        /// <param name="cancellationToken">The cancellation token (if any).</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+        [NotNull]
+        [ItemCanBeNull]
+        Task<TReturn> ExecuteAsync([CanBeNull] TParams parameters = default(TParams), CancellationToken cancellationToken = default(CancellationToken));
+
+        /// <summary>
+        /// Executes the proxy synchronously with the specified parameters.
+        /// </summary>
+        /// <param name="parameters">The parameters (if any).</param>
+        /// <returns>The return type.</returns>
+        [CanBeNull]
+        TReturn Execute([CanBeNull] TParams parameters = default(TParams));
+    }
+}
